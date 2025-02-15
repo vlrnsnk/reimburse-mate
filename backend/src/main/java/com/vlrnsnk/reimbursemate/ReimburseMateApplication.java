a465df2ef23b6efe399fd5d735da13f4b37f066b
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-14 23:31:55 -0500

    Add backend from Spring Initializer

---
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/ReimburseMateApplication.java
@@ -0,0 +1,13 @@
+package com.vlrnsnk.reimbursemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReimburseMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReimburseMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vlrnsnk/reimbursemate/ReimburseMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vlrnsnk.reimbursemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReimburseMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
