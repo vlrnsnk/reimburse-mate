1d2fdade62605c21f5be79847bbf1a28dc804e3b
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-16 10:56:52 -0500

    Implement getAllUsers endpoint

--- backend/src/main/java/com/vlrnsnk/reimbursemate/controller/UserController.java
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.vlrnsnk.reimbursemate.controller;
 
 import com.vlrnsnk.reimbursemate.model.User;
+import com.vlrnsnk.reimbursemate.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,18 @@ import java.util.List;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public ResponseEntity getAllUsers() {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+
+        return ResponseEntity.ok(users);
     }
 
 }
--- backend/src/main/java/com/vlrnsnk/reimbursemate/service/UserService.java
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/service/UserService.java
@@ -1,8 +1,11 @@
 package com.vlrnsnk.reimbursemate.service;
 
+import com.vlrnsnk.reimbursemate.model.User;
 import com.vlrnsnk.reimbursemate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -12,5 +15,10 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    // Get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
