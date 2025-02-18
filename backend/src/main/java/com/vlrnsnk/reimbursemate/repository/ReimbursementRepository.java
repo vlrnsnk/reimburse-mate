ac8532c317eff6f0514c622eebfe308f815591d9
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-17 22:13:37 -0500

    Remove unnecessary findAll from ReimbursementRepository

--- backend/src/main/java/com/vlrnsnk/reimbursemate/repository/ReimbursementRepository.java
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/repository/ReimbursementRepository.java
@@ -8,6 +8,5 @@ import java.util.List;
 public interface ReimbursementRepository extends JpaRepository<Reimbursement, Long> {
 
     List<Reimbursement> findByStatus(Reimbursement.Status status);
-
-    List<Reimbursement> findAll();
+    
 }
