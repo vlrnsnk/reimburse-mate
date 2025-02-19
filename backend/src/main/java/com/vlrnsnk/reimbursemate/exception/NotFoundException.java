091b1d400d55ab0a411ed988d359e55589835c62
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-19 18:25:43 -0500

    Add NotFoundException

---
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.vlrnsnk.reimbursemate.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
