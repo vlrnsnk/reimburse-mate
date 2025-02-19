b49f12850698d6d065efb0c4f275bc5eb00bd6c5
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-19 18:47:03 -0500

    Add GlobalExceptionHandler

---
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/exception/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.vlrnsnk.reimbursemate.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    /**
+     * Handle NotFoundException
+     *
+     * @param e NotFoundException
+     * @return CustomErrorResponse
+     */
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<CustomErrorResponse> handleNotFoundException(NotFoundException e) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.NOT_FOUND.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    /**
+     * Handle ValidationException
+     *
+     * @param e ValidationException
+     * @return CustomErrorResponse
+     */
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<CustomErrorResponse> handleValidationException(ValidationException e) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    /**
+     * Handle Exception
+     *
+     * @param e Exception
+     * @return CustomErrorResponse
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CustomErrorResponse> handleException(Exception e) {
+        CustomErrorResponse errorResponse = new CustomErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+    }
+    
+}
