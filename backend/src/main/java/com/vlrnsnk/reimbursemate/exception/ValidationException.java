9cb64a0f9b86ed8846804f93bef442d4e0ae5cf2
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-19 18:33:49 -0500

    Add ValidationException

---
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.vlrnsnk.reimbursemate.exception;
+
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
