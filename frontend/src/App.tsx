15e37c464dd1826b8c6c5c51aa9facdae0f5ee5a
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-20 18:07:02 -0500

    Add NotFoundPage for catch all invalid routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/public/HomePage/HomePage";
 import { LoginPage } from "./pages/public/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/public/RegisterPage/RegisterPage";
+import { NotFoundPage } from "./pages/public/NotFoundPage/NotFoundPage";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
---
+++ frontend/src/pages/public/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage: React.FC = () => {
+  return (
+    <>
+      <h1>404 - Not Found</h1>
+      <p>This is not the page you are looking for</p>
+    </>
+  );
+};
+
+export { NotFoundPage };
