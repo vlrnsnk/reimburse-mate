bac1c7c2c9e9036f45ce0d4103fbde7861b9b30e
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-21 09:54:53 -0500

    Implement NotFoundPage

--- frontend/src/pages/public/NotFoundPage/NotFoundPage.tsx
+++ frontend/src/pages/public/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,19 @@
+import { PageWrapper } from '@/components/layout/PageWrapper/PageWrapper';
+import { Button } from '@/components/ui/Button/Button';
+
 const NotFoundPage: React.FC = () => {
   return (
-    <>
-      <h1>404 - Not Found</h1>
-      <p>This is not the page you are looking for</p>
-    </>
+    <PageWrapper>
+      <div className="flex flex-col items-center justify-between gap-8">
+        <h1 className="text-4xl font-bold">404</h1>
+        <p className="text-lg">This is not the page you are looking for</p>
+        <Button
+          to="/"
+        >
+          Go Home
+        </Button>
+      </div>
+    </PageWrapper>
   );
 };
 
