2b7b454fd422a861edc6b9ce2a1ee1d249d4c412
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-21 12:04:23 -0500

    Add changing Button style if it leads to current page

--- frontend/src/components/ui/Button/Button.tsx
+++ frontend/src/components/ui/Button/Button.tsx
@@ -1,5 +1,5 @@
 import { ButtonProps } from '@/interfaces/ButtonProps';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Button: React.FC<ButtonProps> = ({
   children,
@@ -8,14 +8,18 @@ const Button: React.FC<ButtonProps> = ({
   to,
   className,
 }) => {
-  const buttonClasses = `px-4 py-2 bg-blue-600 text-gray-100 rounded hover:bg-blue-700 active:bg-green-600 transition-colors duration-300 ease-in-out cursor-pointer disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-700 ${className}`;
+  const buttonClasses = `px-4 py-2 bg-blue-600 text-gray-100 rounded hover:bg-blue-700 active:bg-green-600 active:border-green-600 transition-colors duration-300 ease-in-out cursor-pointer border-blue-600 hover:border-blue-700 disabled:cursor-not-allowed disabled:bg-gray-400 disabled:text-gray-700 ${className}`;
 
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isActiveRoute = to ? location.pathname === to : false;
+  const activeClass = 'bg-gray-50 text-gray-800 border  hover:text-gray-100';
 
   return (
     <button
         onClick={handleClick ? handleClick : to ? () => navigate(to) : undefined}
-        className={`${buttonClasses}`}
+        className={`${buttonClasses} ${isActiveRoute ? activeClass : ''}`}
         disabled={!isActive}
       >
       {children}
