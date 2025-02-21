592ce84a87be2fcc5335c1f876104ee19892242b
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-20 20:22:17 -0500

    Add Header component

---
+++ frontend/src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/logo.svg?react';
+
+const Header: React.FC = () => {
+  return (
+    <header className='bg-gray-50 shadow-sm'>
+      <div className='container mx-auto px-4 py-4 flex flex-col sm:flex-row justify-between items-center text-center md:text-left'>
+        <Link to='/' className='flex flex-col sm:flex-row items-center'>
+          <Logo className='h-10 w-10 text-blue-600' />
+          <span className='mt-2 sm:mt-0 sm:ml-2 text-xl font-semibold text-gray-900'>
+            ReimburseMate
+          </span>
+        </Link>
+        <nav className='mt-4 sm:mt-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4'>
+          <Link to='/login' className='text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out'>
+            Login
+          </Link>
+          <Link to='/register' className='text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out'>
+            Register
+          </Link>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
--- frontend/src/components/Layout/Layout.tsx
+++ frontend/src/components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
+import { Header } from "../Header/Header";
 
 const Layout: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
       <main className="flex flex-col flex-grow items-center justify-center p-4">
         <Outlet />
       </main>
