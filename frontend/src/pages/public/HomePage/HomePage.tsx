fe6eca6e73e57dbb880da37a930e0e3d8861a029
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-20 22:03:38 -0500

    Add HomePage component

--- frontend/src/pages/public/HomePage/HomePage.tsx
+++ frontend/src/pages/public/HomePage/HomePage.tsx
@@ -3,10 +3,19 @@ import Logo from '@/assets/logo.svg?react';
 
 const HomePage: React.FC = () => {
   return (
-    <>
-      <Button to="/login">Login</Button>
-      <Button to="/register">Register</Button>
-    </>
+    <div className="flex flex-col items-center justify-center bg-gray-50">
+      <section className="bg-white p-8 rounded shadow-md text-center">
+        <header>
+          <Logo className="h-20 w-20 mx-auto mb-4 text-blue-600" />
+          <h1 className="text-2xl font-bold text-gray-900 mb-2">ReimburseMate</h1>
+          <p className="text-gray-700 mb-6">Manage your reimbursements with ease.</p>
+        </header>
+        <div className="flex justify-between">
+          <Button to="/login">Login</Button>
+          <Button to="/register">Register</Button>
+        </div>
+      </section>
+    </div>
   );
 };
 
