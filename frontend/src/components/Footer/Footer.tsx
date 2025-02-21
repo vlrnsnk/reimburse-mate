e12d7cb9ba61ff2f94aa0aa45005a6bd238cbac7
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-20 21:22:37 -0500

    Small color changes

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-gray-50 py-6 mt-auto">
+    <footer className="bg-gray-900 text-gray-50 py-6 mt-auto">
       <div className="container mx-auto px-4 text-center text-sm">
         <p>
           &copy; {new Date().getFullYear()} ReimburseMate. All rights reserved.
--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,22 +1,21 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '@/assets/logo.svg?react';
 
 const Header: React.FC = () => {
   return (
-    <header className='bg-gray-50 shadow-sm'>
-      <div className='container mx-auto px-4 py-4 flex flex-col sm:flex-row justify-between items-center text-center md:text-left'>
-        <Link to='/' className='flex flex-col sm:flex-row items-center'>
-          <Logo className='h-10 w-10 text-blue-600' />
-          <span className='mt-2 sm:mt-0 sm:ml-2 text-xl font-semibold text-gray-900'>
+    <header className="bg-gray-50 shadow-sm">
+      <div className="container mx-auto px-4 py-4 flex flex-col sm:flex-row justify-between items-center text-center md:text-left">
+        <Link to="/" className="flex flex-col sm:flex-row items-center">
+          <Logo className="h-10 w-10 text-blue-600" />
+          <span className="mt-2 sm:mt-0 sm:ml-2 text-xl font-semibold text-gray-900">
             ReimburseMate
           </span>
         </Link>
-        <nav className='mt-4 sm:mt-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4'>
-          <Link to='/login' className='text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out' aria-label="Login Page">
+        <nav className="mt-4 sm:mt-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
+          <Link to="/login" className="text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out" aria-label="Login Page">
             Login
           </Link>
-          <Link to='/register' className='text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out' aria-label="Register Page">
+          <Link to="/register" className="text-gray-800 hover:text-gray-900 active:text-blue-600 transition-colors duration-300 ease-in-out" aria-label="Register Page">
             Register
           </Link>
         </nav>
