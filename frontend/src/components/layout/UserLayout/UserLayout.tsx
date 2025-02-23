c018e0e4e118a9179790ded724e65e1d39aecb69
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-23 10:14:31 -0500

    Format code UserLayout

--- frontend/src/components/layout/UserLayout/UserLayout.tsx
+++ frontend/src/components/layout/UserLayout/UserLayout.tsx
@@ -1,41 +1,43 @@
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { Footer } from "@/components/layout/Footer/Footer";
 import Logo from "@/assets/logo.svg?react";
-import { UserRole } from '@/interfaces/UserRole';
-import { Button } from '@/components/ui/Button/Button';
-import toast from 'react-hot-toast';
-import { logoutUser } from '@/services/authService';
+import { UserRole } from "@/interfaces/UserRole";
+import { Button } from "@/components/ui/Button/Button";
+import toast from "react-hot-toast";
+import { logoutUser } from "@/services/authService";
 
 interface UserLayoutProps {
   role: UserRole;
-};
+}
 
 const UserLayout: React.FC<UserLayoutProps> = (role: UserLayoutProps) => {
   const navigate = useNavigate();
 
   const handleLogoutClick = async (e: React.MouseEvent) => {
-      e.preventDefault();
+    e.preventDefault();
 
-      try {
-        const response = await logoutUser();
+    try {
+      const response = await logoutUser();
 
-        const loadingToast = toast.loading('You are being redirected to the login page...');
-        toast.success('Logout successful!');
+      const loadingToast = toast.loading(
+        "You are being redirected to the login page..."
+      );
+      toast.success("Logout successful!");
 
-        localStorage.removeItem('userId');
-        localStorage.removeItem('username');
-        localStorage.removeItem('role');
+      localStorage.removeItem("userId");
+      localStorage.removeItem("username");
+      localStorage.removeItem("role");
 
-        setTimeout(() => {
-          navigate('/login');
-          toast.dismiss(loadingToast);
-        }, 1000);
+      setTimeout(() => {
+        navigate("/login");
+        toast.dismiss(loadingToast);
+      }, 1000);
 
-        console.log(response);
-      } catch (error) {
-        toast.error('Logout failed. Please try again.');
-        console.error('Logout failed:', error);
-      }
+      console.log(response);
+    } catch (error) {
+      toast.error("Logout failed. Please try again.");
+      console.error("Logout failed:", error);
+    }
   };
 
   return (
@@ -50,7 +52,10 @@ const UserLayout: React.FC<UserLayoutProps> = (role: UserLayoutProps) => {
           </Link>
           <nav className="mt-4 sm:mt-0 flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-4">
             <span className="mt-2 sm:mt-0 sm:ml-2 text-md text-gray-900">
-              Hello, <span className="italic text-blue-600">{localStorage.getItem('username')}</span>
+              Hello,{" "}
+              <span className="italic text-blue-600">
+                {localStorage.getItem("username")}
+              </span>
             </span>
             <Link
               to="/logout"
@@ -66,21 +71,9 @@ const UserLayout: React.FC<UserLayoutProps> = (role: UserLayoutProps) => {
       <main className="flex flex-col flex-grow items-center px-4 py-6">
         {role.role === "MANAGER" && (
           <div className="p-4 flex space-x-4 mb-4">
-            <Button
-              to="/manager/reimbursements/my"
-            >
-              My Reimbursements
-            </Button>
-            <Button
-              to="/manager/reimbursements"
-            >
-              Manage Reimbursements
-            </Button>
-            <Button
-              to="/manager/users"
-            >
-              Manage Users
-            </Button>
+            <Button to="/manager/reimbursements/my">My Reimbursements</Button>
+            <Button to="/manager/reimbursements">Manage Reimbursements</Button>
+            <Button to="/manager/users">Manage Users</Button>
           </div>
         )}
         <Outlet />
