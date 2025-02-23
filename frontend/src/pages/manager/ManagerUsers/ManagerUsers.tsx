3e05477e5890a80eea4a04b10b407ec6fdbac605
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-02-23 09:47:21 -0500

    Add loading spinner to manage users view

--- frontend/src/pages/manager/ManagerUsers/ManagerUsers.tsx
+++ frontend/src/pages/manager/ManagerUsers/ManagerUsers.tsx
@@ -2,21 +2,27 @@ import { UserList } from '@/components/users/UserList/UserList';
 import { UserResponse } from '@/interfaces/user';
 import { getUsers } from '@/services/userService';
 import { useEffect, useState } from 'react';
+import { ClipLoader } from 'react-spinners';
 
 const ManagerUsers: React.FC = () => {
   const [users, setUsers] = useState<UserResponse[]>([]);
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const fetchData = async () => {
+    setIsLoading(true);
+
     try {
       const users = await getUsers();
       setUsers(users);
     } catch (error: any) {
       // TODO: Add type guard
       console.log(error.message);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  // TODO: Add isLoading
   useEffect(() => {
     fetchData();
   }, []);
@@ -28,7 +34,11 @@ const ManagerUsers: React.FC = () => {
   return (
     <div className="text-center">
       <h1 className="text-2xl font-semibold mb-12 text-gray-700">Manage Users</h1>
-      {users && users.length > 0
+      {isLoading ? (
+        <div className="flex justify-center">
+          <ClipLoader size={50} color="blue" loading={isLoading} />
+        </div>
+      ) : users && users.length > 0
         ? <UserList
             users={users}
             handleRowDeleted={handleUserDeleted}
