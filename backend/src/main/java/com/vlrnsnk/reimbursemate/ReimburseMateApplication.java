3c03c4ad747c23f03950e6c3bda28de187210f58
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-03-05 23:36:48 -0500

    Configure env variables using local .env file and dotenv-java dependency

--- backend/src/main/java/com/vlrnsnk/reimbursemate/ReimburseMateApplication.java
+++ backend/src/main/java/com/vlrnsnk/reimbursemate/ReimburseMateApplication.java
@@ -1,5 +1,6 @@
 package com.vlrnsnk.reimbursemate;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ReimburseMateApplication {
 
 	public static void main(String[] args) {
+		/*
+		 * Load the environment variables from the .env file
+		 */
+		Dotenv dotenv = Dotenv.load();
+
+		/*
+		 * Set the environment variables to the system properties
+		 */
+		System.setProperty("DB_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DATABASE_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+		/*
+		 * Start the Spring Boot Application
+		 */
 		SpringApplication.run(ReimburseMateApplication.class, args);
 
 		System.out.println("ReimburseMate Application Started");
