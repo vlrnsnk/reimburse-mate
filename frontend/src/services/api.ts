a2e7e90586c5fd02d7655e3e957c84f75518b1e8
Valeriy Anysenko <168164564+vlrnsnk@users.noreply.github.com>
2025-03-08 19:44:34 -0500

    Change backend api URL to Keyob deployment

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios, { AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://reimburse-mate.onrender.com/api/v1',
+  baseURL: 'https://ill-cicely-vlrnsnk-c006a76e.koyeb.app/api/v1',
+  // baseURL: 'https://reimburse-mate.onrender.com/api/v1',
   // baseURL: 'http://localhost:8080/api/v1',
   headers: {
     'Content-Type': 'application/json',
